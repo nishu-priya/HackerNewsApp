92c5683a6c016ee4f215398fff3b92dc370e042b
Nishu Priya <Nishu.Priya@talentica.com>
2018-09-29 12:44:12 +0530

    Initial commit

---
+++ HackerNewsApp/HackerNewsAppTests/HackerNewsAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  HackerNewsAppTests.swift
+//  HackerNewsAppTests
+//
+//  Created by Nishu Priya on 9/29/18.
+//  Copyright © 2018 Nishu Priya. All rights reserved.
+//
+
+import XCTest
+@testable import HackerNewsApp
+
+class HackerNewsAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
